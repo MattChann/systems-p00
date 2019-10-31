b066e3bc7ec9bcf1545fa4407ce74e62e7ca5f4a
Matt <mchan00@stuy.edu>
2019-10-31 11:53:10 -0400

    Finished list headers

--- list_headers.h
+++ list_headers.h
@@ -6,16 +6,16 @@ struct song_node {
 
 struct song_node * insert_front(struct song_node * node, char * name, char * artist);
 
-//insert_order header 
+struct song_node * insert_order(struct song_node * node, char * name, char * artist);
 
 void print_list(struct song_node * node);
 
-//find and return a pointer to a node based on artist and song name 
+struct song_node * find_song(char * artist, char * song);
 
-struct song_node * first_song_of(char * artist);
+struct song_node * find_artist(char * artist);
 
-// return a pointer to random element in the list 
+struct song_node * random_song(struct song_node * node);
 
 struct song_node * remove_node(struct song_node * node, char * name, char * artist);
 
-// free the entire list 
+struct song_node * free_list(struct song_node * node);
