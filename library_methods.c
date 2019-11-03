e7a4391bacc7e64945fae62bea6f37fb7d865875
Matt <mchan00@stuy.edu>
2019-11-03 01:57:02 -0400

    Unified commenting

--- library_methods.c
+++ library_methods.c
@@ -3,43 +3,43 @@
 #include <string.h>
 #include "list_headers.h"
 
-// Add song nodes.
 // matthew
+// Add song nodes
 
-// Search for a song given song and artist name (return a pointer).
-//eric
+// eric
+// Search for a song given song and artist name (return a pointer)
 struct song_node * find(struct song_node * library, char * artist, char * name){
     int index = artist[0] - 'a' + 1;
     if (index < 0) {index = 0;}
     return find_node(&library[index], artist, name);
 }
 
-// Search for an artist.
 // matthew
+// Search for an artist
 
-// Print out all the entries under a certain letter.
-//eric
+// eric
+// Print out all the entries under a certain letter
 void print_letter(struct song_node * library, char letter){
     int index = letter - 'a' + 1;
     if (index < 0) {index = 0;}
     print_list(&library[index]);
 }
 
-// Print out all the songs of a certain artist
 // matthew
+// Print out all the songs of a certain artist
 
-// Print out the entire library.
-//eric
+// eric
+// Print out the entire library
 void print_library(struct song_node * library){
     int i;
     for(i = 0; i < 27; i++) { print_list(&library[i]); }
 }
 
-// Shuffle - print out a series of randomly chosen songs.
 // matthew
+// Shuffle - print out a series of randomly chosen songs
 
+// eric
 // Delete a song
-//eric
 struct song_node * remove_song(struct song_node * library, char * artist, char * name){
     int index = artist[0] - 'a' + 1;
     if (index < 0) {index = 0;}
@@ -47,5 +47,5 @@ struct song_node * remove_song(struct song_node * library, char * artist, char *
     return library;
 }
 
-// Clear the library.
 // matthew
+// Clear the library
