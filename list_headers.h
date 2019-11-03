02fd8036a2aedaabd222ba027b6d792e073792f6
Matt <mchan00@stuy.edu>
2019-11-03 01:43:24 -0400

    Commenting fix

--- list_headers.h
+++ list_headers.h
@@ -4,31 +4,22 @@ struct song_node {
     struct song_node * next;
 };
 
-//eric
 struct song_node * insert_front(struct song_node * node, char * artist, char * name);
 
-//matthew
 int songcmp(struct song_node * node1, struct song_node * node2); // helper function
 struct song_node * insert_order(struct song_node * node, char * artist, char * name);
 
-//eric
 void print_list(struct song_node * node);
 
-//eric
 void print_node(struct song_node * node);
 
-//matthew
 struct song_node * find_node(struct song_node * node, char * artist, char * name);
 
-//eric
 struct song_node * find_artist(struct song_node * node, char * artist);
 
-//matthew
 struct song_node * random_node(struct song_node * node, int seed);
 
-//eric
 int correct_info(struct song_node * node, char * artist, char * name); // helper function
 struct song_node * remove_node(struct song_node * node, char * artist, char * name);
 
-//matthew
 struct song_node * free_list(struct song_node * node);
